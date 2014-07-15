d3a5d017532fc416b21a0a620aaa32842d43e7ed
jinghai <jinghai.xiao@gmail.com>
2014-07-15 17:54:13 +0800

    get_pulltorefresh_code.sh

---
+++ get_pulltorefresh_code.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+#
+#
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+
+URL=https://github.com/chrisbanes/Android-PullToRefresh
+BASE_DIR=/src
+SRC_DIR=$BASE_DIR/Android-PullToRefresh
+
+mkdir -p $BASE_DIR
+
+if [ -d "$SRC_DIR" ]; then
+    cd $SRC_DIR
+    echo "git fetch "
+    git fetch
+else
+    cd $BASE_DIR
+    echo "git clone $URL"
+    git clone $URL
+fi
+
+cd $cur_dir
