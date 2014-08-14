514e89ab0d725b491812bd9e3e5b08cb5a719d3d
jinghai <jinghai.xiao@gmail.com>
2014-08-15 00:21:50 +0800

    tt

--- nginx/install.sh
+++ nginx/install.sh
@@ -4,6 +4,7 @@
 #安装Nginx
 
 echo "正在安装Nginx..."
+#rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 \cp -av nginx.repo /etc/yum.repos.d/
 
 yum -y install nginx
