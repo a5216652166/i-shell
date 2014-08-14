c1318058c3f1aea3fc5b36566a2cec9842e2f7d1
jinghai <jinghai.xiao@gmail.com>
2014-08-14 20:06:07 +0800

    临时提交

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#²Î¿¼£ºhttp://blog.csdn.net/horace20/article/details/26516689
+#²Î¿¼£ºhttp://blog.csdn.net/horace20/article/details/26516689 
 
 #for centos7 http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 wget http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
 yum localinstall mysql-community-release-el6-*.noarch.rpm  
 
 yum install mysql-community-server 
+mysqladmin -u root password 'itserver'
