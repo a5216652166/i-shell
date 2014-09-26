da8042a94a9b56aa1270cf73f7b46aee53a0a58b
jinghai <jinghai.xiao@gmail.com>
2014-09-26 11:14:12 +0800

    iserver_install.sh 调试

--- a-v2.0/iserver_install.sh
+++ a-v2.0/iserver_install.sh
@@ -2,13 +2,37 @@
 #
 set -e
 source ./config.conf
+cur_dir=$(cd "$(dirname "$0")"; pwd)
 
-function getSourceCode() {
-  cur_dir=$(cd "$(dirname "$0")"; pwd)
-  cd $GLOBAL_SOURCECODE_DIR
-  git clone https://github.com/ipetty/i-api
-  git clone https://github.com/ipetty/i-server
-  cd $cur_dir
+## 获取或更新IServer源码
+function getIServerCode() {
+  mkdir -p $GLOBAL_SOURCECODE_DIR
+  if [ ! -d $GLOBAL_SOURCECODE_DIR/i-api ]
+  then
+    cd $GLOBAL_SOURCECODE_DIR
+    git clone https://github.com/ipetty/i-api
+  else
+    cd $GLOBAL_SOURCECODE_DIR/i-api
+    git pull
+  fi
+  if [ ! -d $GLOBAL_SOURCECODE_DIR/i-server ]
+  then
+    cd $GLOBAL_SOURCECODE_DIR
+    git clone https://github.com/ipetty/i-server
+  else
+    cd $GLOBAL_SOURCECODE_DIR/i-server
+    git pull
+  fi
 }
 
-getSourceCode
+## 编译打包IServer
+funcion compileIServer(){
+  cd $GLOBAL_SOURCECODE_DIR/i-api
+  mvn clean install -DskipTests=true 
+  cd $GLOBAL_SOURCECODE_DIR/i-server
+  mvn clean install -DskipTests=true 
+}
+
+getIServerCode
+compileIServer
+cd $cur_dir
