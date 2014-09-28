13bf3cd73048bc3b6e14d1cac295701094bdb9e8
jinghai <jinghai.xiao@gmail.com>
2014-09-28 23:31:29 +0800

    probe

--- a-v2.0/probe_install.sh
+++ a-v2.0/probe_install.sh
@@ -7,6 +7,7 @@ cur_dir=$(cd "$(dirname "$0")"; pwd)
 
 function downloadProbe(){
   mkdir -p $GLOBAL_DOWNLOAD_DIR
+  rm -rf $GLOBAL_DOWNLOAD_DIR/*
   if [ ! -f "$GLOBAL_DOWNLOAD_DIR/probe-${PROBE_VER}.zip" ]; then
     set +e
     export http_proxy="http://202.171.253.134"
@@ -27,13 +28,23 @@ function deployProbe(){
   unzip -oq /tmp/probe.war -d $TOMCAT_HOME/webapps/probe
 }
 
-#function configyProbe(){
-#  #TODO:输入用户名密码
-#
-#  if [ ! -f "$TOMCAT_USER_BACKUP_FILE" ]; then
-#    \cp -av $TOMCAT_USER_FILE $TOMCAT_USER_BACKUP_FILE
-#  fi
-#
+function configyProbe(){
+  #TODO:输入用户名密码
+
+  if [ ! -f "$TOMCAT_USER_BACKUP_FILE" ]; then
+    \cp -av $TOMCAT_USER_FILE $TOMCAT_USER_BACKUP_FILE
+  fi
+
+  echo "<?xml version='1.0' encoding='utf-8'?>" > $TOMCAT_USER_FILE
+  echo '<tomcat-users>' >> $TOMCAT_USER_FILE
+  echo '<role rolename="manager"/>' >> $TOMCAT_USER_FILE
+  echo '<role rolename="tomcat"/>' >> $TOMCAT_USER_FILE
+  echo '<role rolename="poweruser"/>' >> $TOMCAT_USER_FILE
+  echo '<role rolename="poweruserplus"/>' >> $TOMCAT_USER_FILE
+  echo '<role rolename="probeuser"/>' >> $TOMCAT_USER_FILE
+  echo '<user username="admin" password="itserver" roles="tomcat,manager,probeuser,poweruserplus,poweruser"/>' >> $TOMCAT_USER_FILE
+  echo '</tomcat-users>' >> $TOMCAT_USER_FILE
+
 #  (cat > $TOMCAT_USER_FILE <<EOF
 #  <?xml version='1.0' encoding='utf-8'?>
 #  <tomcat-users>
@@ -45,11 +56,11 @@ function deployProbe(){
 #    <user username="admin" password="itserver" roles="tomcat,manager,probeuser,poweruserplus,poweruser"/>
 #  </tomcat-users>
 #  EOF)
-#}
+}
 
 downloadProbe
 deployProbe
-#configyProbe
+configyProbe
 service tomcat restart
 
 cd $cur_dir
