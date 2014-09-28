1d7d8828675d01df3e0c2e1d5cb67ffd96259669
jinghai <jinghai.xiao@gmail.com>
2014-09-28 17:44:16 +0800

    probe安装

--- a-v2.0/probe_install.sh
+++ a-v2.0/probe_install.sh
@@ -5,5 +5,50 @@ set -e
 source ./config.conf
 cur_dir=$(cd "$(dirname "$0")"; pwd)
 
+function downloadProbe(){
+  mkdir -p $GLOBAL_DOWNLOAD_DIR
+  if [ ! -f "$GLOBAL_DOWNLOAD_DIR/probe-${PROBE_VER}.zip" ]; then
+    set +e
+    wget -P $GLOBAL_DOWNLOAD_DIR  "http://psi-probe.googlecode.com/files/probe-${PROBE_VER}.zip"
+    if [ "$?" -eq "1" ]; then
+      rm -rf $GLOBAL_DOWNLOAD_DIR/probe-${PROBE_VER}.zip 
+    fi
+    set -e
+  fi
+}
+
+function deployProbe(){
+  unzip -oq $GLOBAL_DOWNLOAD_DIR/probe-${PROBE_VER}.zip -d /tmp
+  mkdir -p $TOMCAT_HOME/webapps/probe
+  unzip -oq /tmp/probe.war -d $TOMCAT_HOME/webapps/probe
+}
+
+function configyProbe(){
+  #TODO:输入用户名密码
+  TOMCAT_USER_FILE=$TOMCAT_CONFIG_DIR/tomcat-users.xml
+  TOMCAT_USER_BACKUP_FILE= $TOMCAT_CONFIG_DIR/tomcat-users.xml.bak
+
+  if [ ! -f "$TOMCAT_USER_BACKUP_FILE" ]; then
+    \cp -av $TOMCAT_USER_FILE $TOMCAT_USER_BACKUP_FILE
+  fi
+
+  cat >> $TOMCAT_USER_FILE <<EOF
+  <?xml version='1.0' encoding='utf-8'?>
+  <tomcat-users>
+    <role rolename="manager"/>
+    <role rolename="tomcat"/>
+    <role rolename="poweruser"/>
+    <role rolename="poweruserplus"/>
+    <role rolename="probeuser"/>
+    <user username="admin" password="itserver" roles="tomcat,manager,probeuser,poweruserplus,poweruser"/>
+  </tomcat-users>
+  EOF
+}
+
+#downloadProbe
+#deployProbe
+configyProbe
+service tomcat restart
+
 cd $cur_dir
 
