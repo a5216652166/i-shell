c2168c094f0cbd772a755d042152010298b6819c
jinghai <jinghai.xiao@gmail.com>
2014-09-28 23:21:47 +0800

    probe

--- a-v2.0/probe_install.sh
+++ a-v2.0/probe_install.sh
@@ -34,7 +34,7 @@ function configyProbe(){
     \cp -av $TOMCAT_USER_FILE $TOMCAT_USER_BACKUP_FILE
   fi
 
-  cat > $TOMCAT_USER_FILE <<EOF
+  (cat > $TOMCAT_USER_FILE <<EOF
   <?xml version='1.0' encoding='utf-8'?>
   <tomcat-users>
     <role rolename="manager"/>
@@ -44,7 +44,7 @@ function configyProbe(){
     <role rolename="probeuser"/>
     <user username="admin" password="itserver" roles="tomcat,manager,probeuser,poweruserplus,poweruser"/>
   </tomcat-users>
-  EOF
+  EOF)
 }
 
 downloadProbe
