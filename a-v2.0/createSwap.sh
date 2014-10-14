dcfefeb5804b2de7dd793e4716cf0e70443e9879
jinghai <jinghai.xiao@gmail.com>
2014-10-14 14:43:32 +0800

    createSwap.sh

---
+++ a-v2.0/createSwap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+set -e
+
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+
+# 创建分区文件
+# count=SIZE*1024  (size以MB为单位）
+dd if=/dev/zero of=/swap bs=2048 count=2097152
+
+# 格式划分区文件
+mkswap /swap 
+
+# 启用分区
+# 停用分区-->swapoff /swap
+swapon /swap
+
+# 开机自动加载
+echo '/swap swap swap default 0 0' >> /etc/fstab
+
+cd $cur_dir
+
