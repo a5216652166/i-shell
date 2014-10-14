f72a2497cd13feea9c6379d78d391865f3feab75
jinghai <jinghai.xiao@gmail.com>
2014-10-14 17:06:28 +0800

    数据迁移脚本

--- a-v2.0/iserver_data_migration.sh
+++ a-v2.0/iserver_data_migration.sh
@@ -4,6 +4,13 @@ set -e
 source ./config.conf
 cur_dir=$(cd "$(dirname "$0")"; pwd)
 
+mysqldump -h115.29.103.123 -uadmin -p ipetty > /tmp/ipetty.sql
+mysql -e "drop database ipetty;"
+mysql -e "create database if not exists ipetty default charset utf8;"
+mysql ipetty < /tmp/ipetty.sql
+
+
+scp -r $ISERVER_UPLOAD_DIR root@115.29.103.123:/home/data/files
 
 cd $cur_dir
 
